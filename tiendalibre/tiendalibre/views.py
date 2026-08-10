dd585cafa6491091376f8968f04d0c0c751c164c
IvoDKHP <MiscevichIvo@Gmail.Com>
2026-08-10 03:42:58 +0000

    feat: primera vista y template para home y about-me

--- tiendalibre/tiendalibre/views.py
+++ tiendalibre/tiendalibre/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'tiendalibre/home.html')
+
+def about_me(request):
+    return render(request, 'tiendalibre/about_me.html')
